20691f186573e190737e6fd1dfbc84ac0aac80e8
KaichiAkazawa <kic.jump@gmail.com>
2021-02-24 17:24:03 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :owner_users
-  devise_for :customer_users
+  devise_for :customer_users, controllers: {
+    sessions: 'customer_users/sessions',
+    passwords: 'customer_users/passwords',
+    registrations: 'customer_users/registrations'
+  }
+
+  devise_for :owner_users, controllers: {
+    sessions: 'owner_users/sessions',
+    passwords: 'owner_users/passwords',
+    registrations: 'owner_users/registrations'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

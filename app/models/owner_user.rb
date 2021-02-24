e828ae5bc124d16e0787377c2cdb1039a8fbe5f0
KaichiAkazawa <kic.jump@gmail.com>
2021-02-24 17:22:49 +0900

    バリデーション設定

--- app/models/owner_user.rb
+++ app/models/owner_user.rb
@@ -3,4 +3,6 @@ class OwnerUser < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :name, null: false
 end

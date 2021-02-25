5134634ac3fd4d312e51646199766f8fd7adb685
KaichiAkazawa <kic.jump@gmail.com>
2021-02-25 21:30:43 +0900

    バリデーション設定

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,2 +1,12 @@
 class Store < ApplicationRecord
+  belongs_to :owner_user
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :name
+    validates :image
+    validates :adress
+    validates :price
+    validates :store_time
+  end
 end

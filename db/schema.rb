49711a6d4cc6edcfbca4044fe0e845fb4b73ae43
KaichiAkazawa <kic.jump@gmail.com>
2021-02-25 17:55:29 +0900

    storeテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_24_070347) do
+ActiveRecord::Schema.define(version: 2021_02_25_024529) do
 
   create_table "customer_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -38,4 +38,18 @@ ActiveRecord::Schema.define(version: 2021_02_24_070347) do
     t.index ["reset_password_token"], name: "index_owner_users_on_reset_password_token", unique: true
   end
 
+  create_table "stores", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "adress", null: false
+    t.string "station"
+    t.integer "price", null: false
+    t.text "store_time", null: false
+    t.string "link"
+    t.bigint "owner_user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["owner_user_id"], name: "index_stores_on_owner_user_id"
+  end
+
+  add_foreign_key "stores", "owner_users"
 end

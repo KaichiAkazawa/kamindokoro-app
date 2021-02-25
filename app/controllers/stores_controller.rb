6cf929908a9773aa00bd49969d120e6886fd807a
KaichiAkazawa <kic.jump@gmail.com>
2021-02-25 21:31:44 +0900

    新規投稿機能実装

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,4 +1,24 @@
 class StoresController < ApplicationController
   def index
+    @stores = Store.all
+  end
+
+  def new
+    @store = Store.new
+  end
+
+  def create
+    @store = Store.new(store_params)
+    binding.pry
+    if @store.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def store_params
+    params.require(:store).permit(:name, :image, :adress, :station, :price, :store_time, :link).merge(owner_user_id: current_owner_user.id)
   end
 end

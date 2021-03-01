46774f1c2d10f9c6251bd6b1df1744bd6567989d
KaichiAkazawa <kic.jump@gmail.com>
2021-03-01 12:47:20 +0900

    コメントテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_25_092844) do
+ActiveRecord::Schema.define(version: 2021_02_27_102428) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,16 @@ ActiveRecord::Schema.define(version: 2021_02_25_092844) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "text"
+    t.bigint "customer_user_id"
+    t.bigint "store_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["customer_user_id"], name: "index_comments_on_customer_user_id"
+    t.index ["store_id"], name: "index_comments_on_store_id"
   end
 
   create_table "customer_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -73,5 +83,7 @@ ActiveRecord::Schema.define(version: 2021_02_25_092844) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "customer_users"
+  add_foreign_key "comments", "stores"
   add_foreign_key "stores", "owner_users"
 end

ca7cafbe713802012767e700338937c58c81afdc
KaichiAkazawa <kic.jump@gmail.com>
2021-03-02 19:38:11 +0900

    bookmarksコントローラー修正

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -4,11 +4,13 @@ class BookmarksController < ApplicationController
   def create
     @store = Store.find(params[:store_id])
     @bookmark = Bookmark.create(customer_user_id: current_customer_user.id, store_id: @store.id)
+    redirect_to store_path(@store)
   end
 
   def destroy
     @store = Store.find(params[:store_id])
-    @bookmark = Bookmark.find_by(customer_usr_id: current_customer_user.id, store_id: @store.id)
+    @bookmark = Bookmark.find_by(customer_user_id: current_customer_user.id, store_id: @store.id)
     @bookmark.destroy
+    redirect_to store_path(@store)
   end
 end

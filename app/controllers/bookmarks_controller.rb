32d10ea87dbeb814b62a88cb04379ff8542bbdfe
KaichiAkazawa <kic.jump@gmail.com>
2021-03-02 17:20:30 +0900

    bookmarksコントローラー実装

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,14 @@
+class BookmarksController < ApplicationController
+  before_action :authenticate_customer_user!
+
+  def create
+    @store = Store.find(params[:store_id])
+    @bookmark = Bookmark.create(customer_user_id: current_customer_user.id, store_id: @store.id)
+  end
+
+  def destroy
+    @store = Store.find(params[:store_id])
+    @bookmark = Bookmark.find_by(customer_usr_id: current_customer_user.id, store_id: @store.id)
+    @bookmark.destroy
+  end
+end

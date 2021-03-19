dcd8de6f84f5fcc6e76220b53b514952ad2f8a0d
KaichiAkazawa <kic.jump@gmail.com>
2021-03-19 11:36:29 +0900

    ユーザー管理機能テスト修正

--- spec/models/customer_user_spec.rb
+++ spec/models/customer_user_spec.rb
@@ -15,32 +15,32 @@ RSpec.describe CustomerUser, type: :model do
     it 'nameが空では登録できない' do
       @customer_user.name = ""
       @customer_user.valid?
-      expect(@customer_user.errors.full_messages).to include("Name can't be blank")
+      expect(@customer_user.errors.full_messages).to include("名前を入力してください")
     end
     it 'emailが空では登録できない' do
       @customer_user.email = ""
       @customer_user.valid?
-      expect(@customer_user.errors.full_messages).to include("Email can't be blank")
+      expect(@customer_user.errors.full_messages).to include("Eメールを入力してください")
     end
     it 'emailに@がないと登録できない' do
       @customer_user.email = "foobar"
       @customer_user.valid?
-      expect(@customer_user.errors.full_messages).to include("Email is invalid")
+      expect(@customer_user.errors.full_messages).to include("Eメールは不正な値です")
     end
     it 'passwordが空では登録できない' do
       @customer_user.password = ""
       @customer_user.valid?
-      expect(@customer_user.errors.full_messages).to include("Password can't be blank")
+      expect(@customer_user.errors.full_messages).to include("パスワードを入力してください")
     end
     it 'passwordが6文字以上でないと登録できない' do
       @customer_user.password = "a" * 5
       @customer_user.valid?
-      expect(@customer_user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      expect(@customer_user.errors.full_messages).to include("パスワードは6文字以上で入力してください")
     end
     it 'passwordとpassword_confirmationが一致していないと登録できない' do
       @customer_user.password_confirmation = ""
       @customer_user.valid?
-      expect(@customer_user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      expect(@customer_user.errors.full_messages).to include("パスワード（確認用）とパスワードの入力が一致しません")
     end
   end
 end
--- spec/models/owner_user_spec.rb
+++ spec/models/owner_user_spec.rb
@@ -15,32 +15,32 @@ RSpec.describe OwnerUser, type: :model do
     it 'nameが空では登録できない' do
       @owner_user.name = ""
       @owner_user.valid?
-      expect(@owner_user.errors.full_messages).to include("Name can't be blank")
+      expect(@owner_user.errors.full_messages).to include("名前を入力してください")
     end
     it 'emailが空では登録できない' do
       @owner_user.email = ""
       @owner_user.valid?
-      expect(@owner_user.errors.full_messages).to include("Email can't be blank")
+      expect(@owner_user.errors.full_messages).to include("Eメールを入力してください")
     end
     it 'emailに@がないと登録できない' do
       @owner_user.email = "foobar"
       @owner_user.valid?
-      expect(@owner_user.errors.full_messages).to include("Email is invalid")
+      expect(@owner_user.errors.full_messages).to include("Eメールは不正な値です")
     end
     it 'passwordが空では登録できない' do
       @owner_user.password = ""
       @owner_user.valid?
-      expect(@owner_user.errors.full_messages).to include("Password can't be blank")
+      expect(@owner_user.errors.full_messages).to include("パスワードを入力してください")
     end
     it 'passwordが6文字以上でないと登録できない' do
       @owner_user.password = "a" * 5
       @owner_user.valid?
-      expect(@owner_user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      expect(@owner_user.errors.full_messages).to include("パスワードは6文字以上で入力してください")
     end
     it 'passwordとpassword_confirmationが一致していないと登録できない' do
       @owner_user.password_confirmation = ""
       @owner_user.valid?
-      expect(@owner_user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      expect(@owner_user.errors.full_messages).to include("パスワード（確認用）とパスワードの入力が一致しません")
     end
   end
 end

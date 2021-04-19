2b7a94fc474fe0ffb90896de165eaa97c87d485c
KaichiAkazawa <kic.jump@gmail.com>
2021-04-19 20:13:02 +0900

    email一意性テスト追加

--- spec/models/owner_user_spec.rb
+++ spec/models/owner_user_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe OwnerUser, type: :model do
     end
   end
 
-  describe 'カスタマーユーザー新規登録できない' do
+  describe 'オーナーユーザー新規登録できない' do
     it 'nameが空では登録できない' do
       @owner_user.name = ''
       @owner_user.valid?
@@ -26,6 +26,12 @@ RSpec.describe OwnerUser, type: :model do
       @owner_user.email = 'foobar'
       @owner_user.valid?
       expect(@owner_user.errors.full_messages).to include('Eメールは不正な値です')
+    end
+    it 'emailが重複すると登録できない' do
+      @owner_user.save
+      another_user = FactoryBot.build(:owner_user, email: @owner_user.email)
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include('Eメールはすでに存在します')
     end
     it 'passwordが空では登録できない' do
       @owner_user.password = ''

c3aa4662e824cbc651b38f85c6142f76b87e7a37
KaichiAkazawa <kic.jump@gmail.com>
2021-04-23 19:36:44 +0900

    リファクタリング

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,14 +1,19 @@
 class BookmarksController < ApplicationController
   before_action :authenticate_customer_user!
+  before_action :set_store
 
   def create
-    @store = Store.find(params[:store_id])
     Bookmark.create(customer_user_id: current_customer_user.id, store_id: @store.id)
   end
 
   def destroy
-    @store = Store.find(params[:store_id])
     bookmark = Bookmark.find_by(customer_user_id: current_customer_user.id, store_id: @store.id)
     bookmark.destroy
+  end
+
+  private
+
+  def set_store
+    @store = Store.find(params[:store_id])
   end
 end
